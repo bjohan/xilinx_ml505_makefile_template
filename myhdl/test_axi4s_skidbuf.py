b6816f3a926c37dab389e3cf09b5707a9ae14319
bjohan <johan.o.bodin@gmail.com>
2024-02-04 13:45:47 +0100

    Tidied up skidbuf test bench

--- myhdl/test_axi4s_skidbuf.py
+++ myhdl/test_axi4s_skidbuf.py
@@ -1,7 +1,12 @@
 from myhdl import *
-from interface_axi4s import Axi4sInterface
-
+from interface_axi4s import Axi4sInterface, tbTransmitSequence, tbReceiveSequence
 from component_axi4s_skidbuf import axi4s_skidbuf
+
+test_data =     [0xA0, 0xA1, 0xA2, 0xA3, 0xB0, 0xB1, 0xB2, 0xB3, 0xC0, 0xC1, 0xC2, 0xC3]
+test_last =     [0,    0,    0,    0,    0,    0,    0,    1,    0,    0,    0,    1]
+test_delay_in = [0,    0,    1,    1,    0,    0,    3,    0,    0,    0,    0,    0]
+test_delay_out =[1,    2,    3,    1,    0,    0,    3,    0,    0,    0,    0,    0]
+
 
 @block
 def test_axi4s_skidbuf():
@@ -11,6 +16,8 @@ def test_axi4s_skidbuf():
     i = Axi4sInterface(8); 
     o = Axi4sInterface(8);
  
+    axi4s_skidbuf_inst = axi4s_skidbuf(reset, clk, i, o)
+
     @always(delay(10))
     def clkgen():
         if clk:
@@ -18,58 +25,36 @@ def test_axi4s_skidbuf():
         else:
             clk.next = 1
 
-    axi4s_skidbuf_inst = axi4s_skidbuf(reset, clk, i, o)
-
     @instance
     def monitor():
-        for nclk in range(100):
+        for i in range(100):
             yield clk.posedge
-        raise StopSimulation("Too many clk cycles elapsed, increase nclk if needed")
+        print("Simulation did not end successfully")
+        quit(-1)
+
+    @instance
+    def gen_reset():
+        reset.next = 1
+        for i in range(3):
+            yield clk.posedge
+        reset.next = 0
+        yield clk.posedge
 
     @instance
     def read():
-        for j in range(3):
-            yield clk.posedge
-        o.ready.next = 1
-        while True:
-            if not o.valid:
-                yield o.valid.posedge
-            yield clk.posedge
-            o.ready.next = 0;
-            yield clk.posedge
-            o.ready.next = 1;
+        yield reset.negedge
+        yield tbReceiveSequence(clk, o, test_data, test_last, test_delay_out);
+        raise StopSimulation("Simulation ended successfully")
 
     @instance
     def write():
-        print("Synchronous reset")
-        reset.next = 1
-        for j in range(3):
-            yield clk.posedge
-        reset.next = 0
-        print("Waiting 3 clks")
-        for j in range(3):
-            yield clk.posedge
-        print("Starting to transmit")
-        yield clk.posedge
-        for b in [0xA0, 0xA1, 0xA2, 0xA3, 0xB0, 0xB1, 0xB2, 0xB3, 0xC0, 0xC1, 0xC2, 0xC3]:
-            i.valid.next = 1
-            i.data.next = b
-            if b in [0xA3, 0xB3, 0xC3]:
-                i.last.next = 1
-            else:
-                i.last.next = 0
-            yield clk.negedge
-            if not i.ready:
-                yield i.ready.posedge
-            yield clk.posedge
-            i.valid.next = 1
-        print("extra clocks")
-        for j in range(3):
-            yield clk.posedge
-        print("stop simulation")
-        raise StopSimulation("Simulation stopped")
+        yield reset.negedge
+        yield tbTransmitSequence(clk, i, test_data, test_last, test_delay_in);
 
-    return clkgen, axi4s_skidbuf_inst, write, read, monitor
+
+    return clkgen, gen_reset, monitor, axi4s_skidbuf_inst, write, read
+
+
 
 tb = test_axi4s_skidbuf();
 tb.config_sim(trace=True)
