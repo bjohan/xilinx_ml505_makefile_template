1e1ae5559ea0693783ac8b01ca0e4376d69f28e9
bjohan <johan.o.bodin@gmail.com>
2024-02-04 18:37:36 +0100

    Updated application test bench to use new axi4s functions

--- myhdl/test_test_application_str.py
+++ myhdl/test_test_application_str.py
@@ -1,16 +1,14 @@
 from myhdl import *
-from interface_axi4s import Axi4sInterface
-
-
+from interface_axi4s import Axi4sInterface, tbTransmitSequence, tbReceiveSequence
 from component_test_application_str import test_application_str
 
 dataToWrite = [   0xFF,                                            0x01, 0xAB, 0xAC]
-writeDelays = [   0,                                                4,    0,    0,  ]
-writeLast   = [   1,                                                0,    0,    1,  ]
+writeDelays = [   0,                                               4,    0,    0,  ]
+writeLast   = [   1,                                               0,    0,    1,  ]
 
-dataToRead = [      0x01, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4,      0x02, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,    0xAA, 0xAB, 0xAC,     0x02, 0xBA, 0xBB, 0xBC]
-readDelays = [      0,    0,    0,    0,    0,    1,         0,    0,    0,    0,    0,    0,       1,    0,    0,        0,    1,    1,    0,  ]
-readLast   = [      0,    0,    0,    0,    0,    1,         0,    0,    0,    0,    0,    1,       0,    0,    1,        0,    0,    0,    1,  ]
+dataToRead = [    0xFF, 0x01, 0xFF,                                0x01, 0xAB, 0xAC]
+readDelays = [    0,    0,    0,                                   0,    0,    0]
+readLast   = [    1,    0,    1,                                   0,    0,    1]
 
 @block
 def test_test_application_str():
@@ -31,52 +29,32 @@ def test_test_application_str():
 
     @instance
     def monitor():
-        for j in range(100):
-            yield clk.negedge;
-        raise StopSimulation("Ending simulations after 100 cycles");
-
+        for i in range(100):
+            yield clk.posedge
+        print("Simulation did not end successfully")
+        quit(-1)
 
     @instance
-    def read_out():
-        yield reset.negedge;
-        yield clk.posedge
-        for data, last, delay in zip(dataToRead, readLast, readDelays):
-            streamOut.ready.next = 0
-            for j in range(delay):
-                yield clk.posedge
-            streamOut.ready.next = 1
+    def gen_reset():
+        reset.next = 1
+        for i in range(3):
             yield clk.posedge
-            while not streamOut.transacts():
-                yield clk.posedge
-            print("Out Received", streamOut.data, " referece value ", intbv(data), " result:", streamOut.data == data)
-            if last:
-                print()
-        streamOut.ready.next = 0;
+        reset.next = 0
+        yield clk.posedge
+
+    @instance
+    def read():
+        yield reset.negedge
+        yield tbReceiveSequence(clk, streamOut, dataToRead, readLast, readDelays);
+        raise StopSimulation("Simulation ended successfully")
 
     @instance
     def write():
-        reset.next = 1
-        for j in range(3):
-            yield clk.posedge
-        reset.next = 0
-        for j in range(3):
-            yield clk.posedge
-        
-        for data, last, delay in zip(dataToWrite, writeLast, writeDelays):
-            streamIn.valid.next = 0
-            streamIn.data.next = data
-            streamIn.last.next = last
-            for j in range(delay):
-                yield clk.posedge
-            streamIn.valid.next = 1
-            yield clk.posedge
-            while not streamIn.transacts():
-                yield clk.posedge
-        streamIn.valid.next = 0
-        for j in range(3):
-            yield clk.posedge
+        yield reset.negedge
+        yield tbTransmitSequence(clk, streamIn, dataToWrite, writeLast, writeDelays);
 
-    return clkgen, test_application_str_inst, write, read_out, monitor
+
+    return clkgen, gen_reset, monitor, test_application_str_inst, write, read
 
 tb = test_test_application_str();
 tb.config_sim(trace=True)
